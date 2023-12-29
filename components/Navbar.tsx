fa51f9e0874030e9b99d87f367eff2fcfac2ed5f
NIKHIL AGRAWAL <nagrawal203@gmail.com>
2023-12-30 00:16:46 +0530

    Navbar Component

---
+++ components/Navbar.tsx
@@ -0,0 +1,123 @@
+import { useCallback, useEffect, useState } from "react";
+import { BsChevronDown, BsBell, BsSearch } from "react-icons/bs";
+
+import MobileMenu from "./MobileMenu";
+import NavbarItem from "./NavbarItem";
+import AccountMenu from "./AccountMenu";
+import Image from "next/image";
+
+const TOP_OFFSET = 66;
+
+const Navbar = () => {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+  const [showAccountMenu, setShowAccountMenu] = useState(false);
+  const [ShowBackground, setShowBackground] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY >= TOP_OFFSET) {
+        setShowBackground(true);
+      } else {
+        setShowBackground(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const toggleMobileMenu = useCallback(() => {
+    setShowMobileMenu((current) => !current);
+  }, []);
+
+  const toggleAccountMenu = useCallback(() => {
+    setShowAccountMenu((current) => !current);
+  }, []);
+
+  return (
+    <nav className="w-full fixed z-40">
+      <div
+        className={`
+        px-4
+        md:px-16
+        py-6
+        flex
+        flex-row
+        items-center
+        transition
+        duration-500
+        ${ShowBackground ? 'bg-zinc-900/[0.9]' : ''}
+        `}
+      >
+        <Image
+          className="h-7 lg:h-10"
+          src="/images/logo.png"
+          alt="logo"
+          width={100}
+          height={100}
+        />
+        <div
+          className="
+            flex-row
+            ml-8
+            gap-7
+            hidden
+            lg:flex
+        "
+        >
+          <NavbarItem label="Home" />
+          <NavbarItem label="Series" />
+          <NavbarItem label="Films" />
+          <NavbarItem label="New & Popular" />
+          <NavbarItem label="My list" />
+          <NavbarItem label="Browse by languages" />
+        </div>
+        <div
+          onClick={toggleMobileMenu}
+          className="lg:hidden flex justify-between flex-row items-center gap-2 ml-8 cursor-pointer relative"
+        >
+          <p className="text-sm">Browse</p>
+          <BsChevronDown
+            className={`text-sm transition ease-in-out duration-200 ${
+              showMobileMenu ? "rotate-180" : "rotate-0"
+            }`}
+          />
+          <MobileMenu visible={showMobileMenu} />
+        </div>
+        <div className="flex flex-row ml-auto gap-7 items-center">
+          <div className="text-gray-200 hover:text-gray-300 cursor-pointer">
+            <BsSearch size={20} />
+          </div>
+          <div className="text-gray-200 hover:text-gray-300 cursor-pointer">
+            <BsBell size={20} />
+          </div>
+
+          <div
+            onClick={toggleAccountMenu}
+            className="flex flex-row items-center gap-2 cursor-pointer relative"
+          >
+            <div className="w-6 h-6 lg:w-10 lg:h-10 rounded-md overflow-hidden">
+              <Image
+                src="/images/default-red.png"
+                alt="profile"
+                width={100}
+                height={100}
+              />
+            </div>
+            <BsChevronDown
+              className={`text-l transition ease-in-out duration-200 ${
+                showAccountMenu ? "rotate-180" : "rotate-0"
+              }`}
+            />
+            <AccountMenu visible={showAccountMenu} />
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import useCurrentUser from "@/hooks/useCurrentUser";
+import Navbar from "@/components/Navbar";
 import { NextPageContext } from "next";
-import { getSession, signOut } from "next-auth/react";
+import { getSession } from "next-auth/react";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -14,22 +14,14 @@ export async function getServerSideProps(context: NextPageContext) {
   }
 
   return {
-    props: { },
+    props: {},
   };
 }
 
 export default function Home() {
-  const {data: user} = useCurrentUser();
   return (
     <>
-      <h1 className="text-4xl text-green-400">Netflix Clone</h1>
-      <p>Logged in as: {user?.name}</p>
-      <button
-        className="text-black font-bold text-2xl h-10 w-full bg-white"
-        onClick={() => signOut()}
-      >
-        Logout!
-      </button>
+      <Navbar />
     </>
   );
 }
