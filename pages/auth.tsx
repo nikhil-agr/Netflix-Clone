09209a1ccb1b377bc43f835d50cb628cd25d41c6
NIKHIL AGRAWAL <nagrawal203@gmail.com>
2023-12-29 23:02:46 +0530

    Protecting Routes and Profile Screen

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -2,13 +2,11 @@ import Input from "@/components/Input";
 import axios from "axios";
 import { signIn } from "next-auth/react";
 import Image from "next/image";
-import { useRouter } from "next/router";
 import { useCallback, useState } from "react";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 
 const Auth = () => {
-  const router = useRouter();
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
@@ -26,14 +24,12 @@ const Auth = () => {
       await signIn("credentials", {
         email,
         password,
-        redirect: false,
-        callbackUrl: "/",
+        callbackUrl: "/profiles",
       });
-      router.push("/");
     } catch (error) {
       console.log(error);
     }
-  }, [email, password, router]);
+  }, [email, password]);
 
   const register = useCallback(async () => {
     try {
@@ -98,7 +94,7 @@ const Auth = () => {
             <div className="flex flex-row items-center gap-4 mt-8 justify-center">
               <div
                 onClick={() => {
-                  signIn("google", { callbackUrl: "/" });
+                  signIn("google", { callbackUrl: "/profiles" });
                 }}
                 className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
               >
@@ -106,7 +102,7 @@ const Auth = () => {
               </div>
               <div
                 onClick={() => {
-                  signIn("github", { callbackUrl: "/" });
+                  signIn("github", { callbackUrl: "/profiles" });
                 }}
                 className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
               >
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,35 @@
+import useCurrentUser from "@/hooks/useCurrentUser";
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: { },
+  };
+}
+
 export default function Home() {
+  const {data: user} = useCurrentUser();
   return (
     <>
-    <h1>Netflix Clone</h1>
+      <h1 className="text-4xl text-green-400">Netflix Clone</h1>
+      <p>Logged in as: {user?.name}</p>
+      <button
+        className="text-black font-bold text-2xl h-10 w-full bg-white"
+        onClick={() => signOut()}
+      >
+        Logout!
+      </button>
     </>
-  )
+  );
 }
